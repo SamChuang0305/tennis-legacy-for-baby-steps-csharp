aa1f5019394186d904d0f12f92df7a4876e84882
Sam Chuang <samchuang@myhost.idv.tw>
2021-02-06 13:01:15 +0800

    (00:00:00 ~ 00:03:45) refactor: extract fields and variables

--- Tennis/Tennis.cs
+++ Tennis/Tennis.cs
@@ -4,80 +4,101 @@
     {
         private int _firstPlayerScoreTimes;
         private int _secondPlayerScoreTimes;
+        private string _firstPlayerName;
+        private string _secondPlayerName;
+
+        private string _love;
+        private string _fifteen;
+        private string _thirty;
+        private string _forty;
+
+        private string _all = "all";
+        private string _deuce = "deuce";
+
+        public Tennis()
+        {
+            _firstPlayerName = "Joey";
+            _secondPlayerName = "Tom";
+        }
 
         public string Score()
         {
+            _love = "love";
+            _fifteen = "fifteen";
+            _thirty = "thirty";
+            _forty = "forty";
             if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 0)
             {
-                return "love all";
+                return _love + " " + _all;
             }
             if (_firstPlayerScoreTimes == 1 && _secondPlayerScoreTimes == 0)
             {
-                return "fifteen love";
+                return _fifteen + " " + _love;
             }
+
             if (_firstPlayerScoreTimes == 2 && _secondPlayerScoreTimes == 0)
             {
-                return "thirty love";
+                return _thirty + " " + _love;
             }
             if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 0)
             {
-                return "forty love";
+                return _forty + " " + _love;
             }
             if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 1)
             {
-                return "love fifteen";
+                return _love + " " + _fifteen;
             }
             if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 2)
             {
-                return "love thirty";
+                return _love + " " + _thirty;
             }
             if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 3)
             {
-                return "love forty";
+                return _love + " " + _forty;
             }
             if (_firstPlayerScoreTimes == 1 && _secondPlayerScoreTimes == 1)
             {
-                return "fifteen all";
+                return _fifteen + " " + _all;
             }
             if (_firstPlayerScoreTimes == 2 && _secondPlayerScoreTimes == 2)
             {
-                return "thirty all";
+                return _thirty + " " + _all;
             }
             if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 3)
             {
-                return "deuce";
+                return _deuce;
             }
             if (_firstPlayerScoreTimes == 4 && _secondPlayerScoreTimes == 4)
             {
-                return "deuce";
+                return _deuce;
             }
             if (_firstPlayerScoreTimes == 4 && _secondPlayerScoreTimes == 3)
             {
-                return "Joey adv";
+                return _firstPlayerName + " adv";
             }
             if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 4)
             {
-                return "Tom adv";
+                return _secondPlayerName + " adv";
             }
             if (_firstPlayerScoreTimes == 5 && _secondPlayerScoreTimes == 3)
             {
-                return "Joey win";
+                return _firstPlayerName + " win";
             }
             if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 5)
             {
-                return "Tom win";
+                return _secondPlayerName + " win";
             }
             return null;
         }
 
         public void firstPlayerScore()
         {
-            _firstPlayerScoreTimes++;
+            this._firstPlayerScoreTimes++;
         }
 
         public void secondPlayerScore()
         {
-            _secondPlayerScoreTimes++;
+            this._secondPlayerScoreTimes++;
         }
     }
 }
